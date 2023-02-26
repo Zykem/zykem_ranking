537f5ecc22ae79f1cd6f90da63e368ee90cb28ad
zykem#2682 <86602828+Zykem@users.noreply.github.com>
2023-02-27 00:51:05 +0100

    Create sv_config.lua

---
+++ sv_config.lua
@@ -0,0 +1,12 @@
+svcfg = {}
+
+
+svcfg.webhooks = {
+
+    playtime = "https://discord.com/api/webhooks/",
+    kills = "https://discord.com/api/webhooks/",
+    deaths = "https://discord.com/api/webhooks/",
+    money = "https://discord.com/api/webhooks/",
+    serverLogo = "https://cdn.discordapp.com/attachments/1037431822960820226/1079200906786127882/DSC_6372-1200x630.jpg" -- Displayed in Discord Embed
+
+}
